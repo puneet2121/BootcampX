89e797ea20e53b2b2843d40164285934d6708c62
puneet2121 <bedipuneet21@gmail.com>
2022-05-31 03:33:08 +0000

    adding assignment tables

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -5,6 +5,11 @@ CREATE TABLE cohort (
   end_date DATE
 );
 
-CREATE TABLE students (id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255), phone VARCHAR(32), github VARCHAR(255), start_date DATE, end_date DATE, cohort_id INTEGER);
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
+  phone VARCHAR(32),
+  github VARCHAR(255), start_date DATE, end_date DATE, cohort_id INTEGER REFERENCES cohort(id) ON DELETE CASCADE);
 
 
