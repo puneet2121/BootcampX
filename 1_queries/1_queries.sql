1d01eac68c1956b2087b66ac006c853c1f418f87
puneet2121 <bedipuneet21@gmail.com>
2022-05-31 07:13:46 +0000

    adding join assignment to repo

--- 1_queries/1_queries.sql
+++ 1_queries/1_queries.sql
@@ -28,6 +28,6 @@ ORDER BY cohort_id;
 
 -- Graduates without a linked github account
 
-SELECT name, email,phone 
+SELECT name, email, phone 
 FROM students
 WHERE github IS NULL AND end_date IS NOT NULL;
